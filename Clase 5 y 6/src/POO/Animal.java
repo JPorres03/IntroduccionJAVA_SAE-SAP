032eae5ef45452b87266e9660ecefbafa95906f3
Josue_Porres <3524056350101@ingenieria.usac.edu.gt>
2026-08-04 13:49:25 -0600

    feat: Clase 5 y 6

---
+++ Clase 5 y 6/src/POO/Animal.java
@@ -0,0 +1,12 @@
+package POO;
+
+/**
+ * @author jdpor
+ */
+public class Animal {
+    
+    public void hablar(){
+        System.out.println("El animal hace sonido");
+    }
+}
+
---
+++ Clase 5 y 6/src/POO/Perro.java
@@ -0,0 +1,12 @@
+package POO;
+
+/**
+ *
+ * @author jdpor
+ */
+public class Perro extends Animal{
+    @Override
+    public void hablar(){
+        System.out.println("El perro ladra: GUAU GUAU");
+    }
+}
